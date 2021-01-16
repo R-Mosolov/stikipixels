767fafe006d7196cfceb711b7c067cf7caefb774
Roman Mosolov <R.V.Mosolov@ya.ru>
2021-01-16 19:22:31 +0300

    Clear unused dependencies

--- src/pages/main.page.js
+++ src/pages/main.page.js
@@ -1,59 +1,12 @@
 import React, { Component } from 'react';
-import { withStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import Box from '@material-ui/core/Box';
-import MuiDialogTitle from '@material-ui/core/DialogTitle';
-import MuiDialogContent from '@material-ui/core/DialogContent';
-import MuiDialogActions from '@material-ui/core/DialogActions';
-import IconButton from '@material-ui/core/IconButton';
-import CloseIcon from '@material-ui/icons/Close';
-import Typography from '@material-ui/core/Typography';
 
 import PhotoCard from '../components/photo-card';
 
 // TODO: Move ACCESS_TOKEN to .env module before production
 const ACCESS_TOKEN = 'staCw-UZQJJApKS9QthfvDP6QIXYUmlQnfnkwgkKh8o';
 const URL = 'https://api.unsplash.com/photos/?per_page=12&client_id=' + ACCESS_TOKEN;
-
-const styles = (theme) => ({
-  root: {
-    margin: 0,
-    padding: theme.spacing(2),
-  },
-  closeButton: {
-    position: 'absolute',
-    right: theme.spacing(1),
-    top: theme.spacing(1),
-    color: theme.palette.grey[500],
-  },
-});
-
-const DialogTitle = withStyles(styles)((props) => {
-  const { children, classes, onClose, ...other } = props;
-  return (
-    <MuiDialogTitle disableTypography className={classes.root} {...other}>
-      <Typography variant="h6">{children}</Typography>
-      {onClose ? (
-        <IconButton aria-label="close" className={classes.closeButton} onClick={onClose}>
-          <CloseIcon />
-        </IconButton>
-      ) : null}
-    </MuiDialogTitle>
-  );
-});
-
-const DialogContent = withStyles((theme) => ({
-  root: {
-    padding: theme.spacing(2),
-  },
-}))(MuiDialogContent);
-
-const DialogActions = withStyles((theme) => ({
-  root: {
-    margin: 0,
-    padding: theme.spacing(1),
-  },
-}))(MuiDialogActions);
 
 export default class MainPage extends Component {
   constructor(props) {
--- src/pages/photo.page.js
+++ src/pages/photo.page.js
@@ -28,9 +28,7 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-export default function PhotoPage(data) {
-  const { imageURL } = data;
-
+export default function PhotoPage() {
   const classes = useStyles();
 
   return (
@@ -53,7 +51,10 @@ export default function PhotoPage(data) {
           />
           <CardMedia
             className={classes.media}
-            image={"https://helpx.adobe.com/content/dam/help/en/stock/how-to/visual-reverse-image-search/jcr_content/main-pars/image/visual-reverse-image-search-v2_intro.jpg"}
+            image={
+              "https://helpx.adobe.com/content/dam/help/en/stock/how-to/visual-reverse-image-search"
+              + "/jcr_content/main-pars/image/visual-reverse-image-search-v2_intro.jpg"
+            }
             title="Paella dish"
           />
           <CardContent>
